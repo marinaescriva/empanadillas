a5231c7a2303bdde3f92435c5d002dbf2a347122
marinaescriva <marinaescriva.24@gamil.com>
2024-02-09 16:38:36 +0100

    developing the main idea to start the while

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-export const fn = (/* argumentos de la función */) => {
+export const fn = (a, b, c) => {
+
+   while((a+b+c)<40 && ((a+b+c)%3 === 0)){
+
+      let ab = (a + b)/2;
+      let ac = (a + c)/2;
+      let bc = (b + c)/2;
+
+      a = 12;
+      b = 14;
+      c = 16;
+
+      ab = 13;
+      ac = 14;
+      bc = 15;
+
+
+   }
+   
+   
    // Cuerpo de la función: Completa la lógica de la función aquí.
 };
