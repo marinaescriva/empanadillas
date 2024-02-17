b156145b202e72bd6307481da654f32706cac5d6
marinaescriva <marinaescriva.24@gamil.com>
2024-02-17 12:52:23 +0100

    all solved except 11,10,9

--- main.js
+++ main.js
@@ -20,27 +20,55 @@ export const fn = (empA, empB, empC) => {
   }
 
 
-  if (empC < empA) { //3,3,0 o 2,0,1
+  if (empC < empA) { //11,10,9
 
-    let empNoMix = (empA - empC) // 2
+    let empNoAC = empA - (empA - empC)  //11- (11-9)  2
+    
+     if(empC == 0){
+        let empNoMix = empA - empB 
+        let empMix = (empA - empNoMix)*2
+        console.log(empMix)
+       let pFinal = (empMix/3)*13
+       return(pFinal)
+      }
+    
+    let empAC = (empA - (empNoAC))*2 // 11- 2 
+    let empMix = (empB + empAC) // 10 + 9
 
-    if (empNoMix < 0) {
-      let empNoMix = -(empNoMix)
-    }
+    let X = empMix %3 // 1
 
-    let empMix = (empC * 2) // 18
-
-    let empX = (empB + empMix) // 10+ 18 (b + ac)
-    let empY = (empX % 3) // 1 de tipo b no mezclada
-
-    let pPago = ((empX - (empY)) / 3) * 14 //(27/3)  *14
-    let sPago = ((empNoMix + (empY)) / 3) * 13 //pagas la mezclada de ab, osea la empY(1) mexclada con una empNomix que es de tipo a.
-    // empnomix son siempre a , y lo que sobre sin mezclar es tipo b 
+    let pPago = ((empMix - X)/3)*14
+    let sPago = ((X + empNoAC)/3)*14
 
     let pFinal = pPago + sPago
-    return (pFinal)
+    // let pFinal = ((empMix + empNoAC)/3)*14 
+    
+    return(pFinal)
+    
 
   }
+
+  // if (empC < empA) {
+
+  //   let empNoMix = (empA - empC) // 2
+
+  //   // if (empNoMix === 0) {
+  //   //   let empNoAB = -(empA - empB)  ESTO ES LO QUE NO PASA POR 3,3,0 Y 2,0,1
+  //   // }
+
+  //   let empMix = (empC * 2) // 18
+
+  //   let empX = (empB + empMix) // 10+ 18 (b + ac)
+  //   let empY = (empX % 3) // 1 de tipo b no mezclada
+
+  //   let pPago = ((empX - (empY)) / 3) * 14 //(27/3)  *14
+  //   let sPago = ((empNoMix + (empY)) / 3) * 13 //pagas la mezclada de ab, osea la empY(1) mexclada con una empNomix que es de tipo a.
+  //   // empnomix son siempre a , y lo que sobre sin mezclar es tipo b 
+
+  //   let pFinal = pPago + sPago
+  //   return (pFinal)
+
+  // }
 
 
   if ((empC > empA)) {
