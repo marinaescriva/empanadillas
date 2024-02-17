1def1e86139e4c8de51bc82d4f9ccabf6cc6edbd
marinaescriva <marinaescriva.24@gamil.com>
2024-02-17 13:15:27 +0100

    editing when  A>C

--- main.js
+++ main.js
@@ -20,68 +20,65 @@ export const fn = (empA, empB, empC) => {
   }
 
 
-  if (empC < empA) { //11,10,9
+  if (empC < empA) {
 
-    let empNoAC = empA - (empA - empC)  //11- (11-9)  2
-    
-     if(empC == 0){
-        let empNoMix = empA - empB 
-        let empMix = (empA - empNoMix)*2
-        console.log(empMix)
-       let pFinal = (empMix/3)*13
-       return(pFinal)
+    let empAC = empA - (empA - empC)
+
+    if (empC == 0) {
+      let empNoMix = empA - empB
+      let empMix = (empA - empNoMix) * 2
+      console.log(empMix)
+      let pFinal = (empMix / 3) * 13
+      return (pFinal)
+    }
+    if (empC != 0) {
+
+      let empNoAC = (empA - empAC)
+      console.log(empNoAC, "lasnoAC osea a son 2")
+      let empMix = empB + empAC
+      console.log(empMix, "las mix son las 9noac mas las b")
+
+      console.log(empMix % 3, "elmodulo de mix es 19-18 q si es div/3")
+
+      let pPago = (((empMix) - (empMix % 3)) / 3) * 14
+      console.log(pPago, "primerpago")
+
+
+      if (empMix > 3) {
+
+        let empX = (empB + empMix)
+        console.log(empMix, "q son mix 19")
+        let empY = (empMix % 3)
+        console.log(empY, "DEBESER 1 b q no se ha mezclado aun")
+        let sPago = (empNoAC - (empNoAC % 3) / 3) * 12
+
+        let tPago = ((empY + (empNoAC % 3)) / 3) * 14
+        console.log(tPago)
+
+        let pFinal = pPago + sPago + tPago
+        return (pFinal)
+
       }
-    
-    let empAC = (empA - (empNoAC))*2 // 11- 2 
-    let empMix = (empB + empAC) // 10 + 9
 
-    let X = empMix %3 // 1
+      let sPago = ((empNoAC + empMix + (empMix % 3)) / 3) * 14
+      console.log(sPago, "elsegundopago es esto")
 
-    let pPago = ((empMix - X)/3)*14
-    let sPago = ((X + empNoAC)/3)*14
-
-    let pFinal = pPago + sPago
-    // let pFinal = ((empMix + empNoAC)/3)*14 
-    
-    return(pFinal)
-    
-
+      let pFinal = pPago + sPago
+      return (pFinal)
+    }
   }
 
-  // if (empC < empA) {
-
-  //   let empNoMix = (empA - empC) // 2
-
-  //   // if (empNoMix === 0) {
-  //   //   let empNoAB = -(empA - empB)  ESTO ES LO QUE NO PASA POR 3,3,0 Y 2,0,1
-  //   // }
-
-  //   let empMix = (empC * 2) // 18
-
-  //   let empX = (empB + empMix) // 10+ 18 (b + ac)
-  //   let empY = (empX % 3) // 1 de tipo b no mezclada
-
-  //   let pPago = ((empX - (empY)) / 3) * 14 //(27/3)  *14
-  //   let sPago = ((empNoMix + (empY)) / 3) * 13 //pagas la mezclada de ab, osea la empY(1) mexclada con una empNomix que es de tipo a.
-  //   // empnomix son siempre a , y lo que sobre sin mezclar es tipo b 
-
-  //   let pFinal = pPago + sPago
-  //   return (pFinal)
-
-  // }
 
 
   if ((empC > empA)) {
 
 
-    let empNoMix = (empC - empA) // 11-9
-    // if(empNoMix<0){ 
-    //   let empNoMix = -(empNoMix)
-    // }
-    let empMix = (empA * 2) // 9*2 18
+    let empNoMix = (empC - empA)
 
-    let empX = (empB + empMix)  //28
-    let empY = (empX % 3) //1
+    let empMix = (empA * 2)
+
+    let empX = (empB + empMix)
+    let empY = (empX % 3)
 
     let pPago = ((empX - (empY)) / 3) * 14
     let sPago = ((empNoMix + (empY)) / 3) * 16
